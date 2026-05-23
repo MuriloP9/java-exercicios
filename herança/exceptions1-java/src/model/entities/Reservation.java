c96ac0dd0158c5118bbb33b365e81ce52461eb1e
Murilo <murilopereiranunesrosa@gmail.com>
2026-05-22 23:44:38 -0300

    Very bad solution

---
+++ herança/exceptions1-java/src/model/entities/Reservation.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(Date checkOut, Date cheIn, Integer roomNumber) {
+        this.checkOut = checkOut;
+        this.checkIn = cheIn;
+        this.roomNumber = roomNumber;
+    }
+
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+
+    public Date getCheIn() {
+        return checkIn;
+    }
+
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return  TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString(){
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkOut)
+                + duration()
+                + " nights ";
+    }
+
+}
---
+++ herança/par_impar/src/Finally.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Finally {
+    public static void main(String[] args) {
+        File file = new File("E:/in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (FileNotFoundException e)  {
+            System.out.println("Error or opening file: " + e.getMessage());
+        } finally {
+            if (sc != null){
+                sc.close();
+            }
+            System.out.println("Finally block executed");
+        }
+
+    }
+
+
+}
